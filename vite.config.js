60d1e90d16c59a1c3d40931ebf88b1f6f65e13e3
jae kaplan <me@jkap.io>
2023-01-20 11:25:39 -0500

    minor build changes

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
+
+export default defineConfig({
+    plugins: [ViteMinifyPlugin({})],
+    build: {
+        sourcemap: true,
+    },
+});
